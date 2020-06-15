e7b2d4ba3316a2b78fe3b7cf859be22556cc0cbc
PeterNguyen <peter.nguyen@vmodev.com>
2020-06-15 14:46:22 +0700

    set up project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+import App from 'components/App';
+import rootRouter from './reducers';
+import serviceWorker from './serviceWorker';
+
+const store = createStore(rootRouter);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
